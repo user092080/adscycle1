a06c83f9f92695de3f740e030b57a95ea02b7d89
Allen <147998993+user092080@users.noreply.github.com>
2023-11-20 14:08:31 +0530

    Update circle.c

--- circle.c
+++ circle.c
@@ -1,177 +1,148 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-
-// Structure to represent a circular queue
-struct CircularQueue {
-    int *arr;
-    int front, rear;
-    int capacity;
-    bool isFull;
-};
-
-// Function to create a circular queue
-struct CircularQueue* createCircularQueue(int capacity) {
-    struct CircularQueue* queue = (struct CircularQueue*)malloc(sizeof(struct CircularQueue));
-    if (queue == NULL) {
-        printf("Memory allocation failed.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    queue->arr = (int*)malloc(capacity * sizeof(int));
-    if (queue->arr == NULL) {
-        printf("Memory allocation failed.\n");
-        free(queue);
-        exit(EXIT_FAILURE);
-    }
-
-    queue->capacity = capacity;
-    queue->front = -1;
-    queue->rear = -1;
-    queue->isFull = false;
-    return queue;
-}
-
-// Function to check if the circular queue is empty
-bool isEmpty(struct CircularQueue* queue) {
-    return (queue->front == -1);
-}
-
-// Function to check if the circular queue is full
-bool isFull(struct CircularQueue* queue) {
-    return (queue->isFull);
-}
-
-// Function to enqueue an element
-void enqueue(struct CircularQueue* queue, int data) {
-    if (isFull(queue)) {
-        printf("Queue is full. Unable to enqueue.\n");
-        return;
-    }
-
-    if (isEmpty(queue)) {
-        queue->front = 0;
-    }
-
-    queue->rear = (queue->rear + 1) % queue->capacity;
-    queue->arr[queue->rear] = data;
-
-    if (queue->front == ((queue->rear + 1) % queue->capacity)) {
-        queue->isFull = true;
-    }
-}
-
-// Function to dequeue an element
-void dequeue(struct CircularQueue* queue) {
-    if (isEmpty(queue)) {
-        printf("Queue is empty. Unable to dequeue.\n");
-        return;
-    }
-
-    if (queue->front == queue->rear) {
-        queue->front = -1;
-        queue->rear = -1;
-        queue->isFull = false;
-    } else {
-        queue->front = (queue->front + 1) % queue->capacity;
-    }
-}
-
-// Function to search for an element in the queue
-int search(struct CircularQueue* queue, int value) {
-    if (isEmpty(queue)) {
-        printf("Queue is empty. Unable to search.\n");
-        return -1;
-    }
-
-    int index = queue->front;
-    while (index != queue->rear) {
-        if (queue->arr[index] == value) {
-            return index;
-        }
-        index = (index + 1) % queue->capacity;
-    }
-
-    if (queue->arr[index] == value) {
-        return index;
-    }
-
-    printf("Value not found in the queue.\n");
-    return -1;
-}
-
-// Function to display the queue
-void displayQueue(struct CircularQueue* queue) {
-    if (isEmpty(queue)) {
-        printf("Queue is empty.\n");
-        return;
-    }
-
-    printf("Queue: ");
-    int index = queue->front;
-    while (index != queue->rear) {
-        printf("%d -> ", queue->arr[index]);
-        index = (index + 1) % queue->capacity;
-    }
-    printf("%d -> NULL\n", queue->arr[index]);
-}
-
-// Function to free the allocated memory for the queue
-void freeQueue(struct CircularQueue* queue) {
-    free(queue->arr);
-    free(queue);
-}
+int *queue, front = -1, rear = -1, size;
+void initializeQueue();
+int isFull(), isEmpty();
+void enqueue(int element);
+int dequeue();
+int searchElement(int element);
+void displayQueue();
 
 int main() {
-    int capacity, choice, data, searchValue;
+    int choice, element, searchResult;
 
-    printf("Enter the capacity of the circular queue: ");
-    scanf("%d", &capacity);
+    printf("Enter the size of Queue: ");
+    scanf("%d", &size);
 
-    struct CircularQueue* queue = createCircularQueue(capacity);
+    initializeQueue();
 
-    while (1) {
-        printf("\nMenu:\n");
-        printf("1. Enqueue\n");
-        printf("2. Dequeue\n");
-        printf("3. Search\n");
-        printf("4. Display the queue\n");
+    do {
+        printf("1. Enqueue element)\n");
+        printf("2. Dequeue element)\n");
+        printf("3. Search element\n");
+        printf("4. Display Queue\n");
         printf("5. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                printf("Enter value to enqueue: ");
-                scanf("%d", &data);
-                enqueue(queue, data);
+                printf("Enter element to enter: ");
+                scanf("%d", &element);
+                enqueue(element);
                 break;
 
             case 2:
-                dequeue(queue);
+                dequeue();
                 break;
 
             case 3:
-                printf("Enter value to search: ");
-                scanf("%d", &searchValue);
-                int position = search(queue, searchValue);
-                if (position != -1) {
-                    printf("Value %d found at position %d.\n", searchValue, position);
+                printf("Enter element to search: ");
+                scanf("%d", &element);
+                searchResult = searchElement(element);
+                if (searchResult != -1) {
+                    printf("%d found at position %d\n", element, searchResult);
+                } else {
+                    printf("%d not found \n", element);
                 }
                 break;
 
             case 4:
-                displayQueue(queue);
+                displayQueue();
                 break;
 
             case 5:
-                freeQueue(queue);
-                exit(EXIT_SUCCESS);
+                break;
 
             default:
-                printf("Invalid choice. Please try again.\n");
+                printf("Invalid choice. Please enter a valid option.\n");
         }
-    }
+    } while (choice != 0);
+
+    free(queue);
 
     return 0;
+}
+
+
+void initializeQueue() {
+    queue = (int *)malloc(size * sizeof(int));
+}
+
+
+int isFull() {
+    return (front == (rear + 1) % size);
+}
+int isEmpty() {
+    return (front == -1 && rear == -1);
+}
+
+void enqueue(int element) {
+    if (isFull()) {
+        printf("Queue is full\n");
+        return;
+    }
+
+    if (isEmpty()) {
+        front = rear = 0;
+    } else {
+        rear = (rear + 1) % size;
+    }
+
+    queue[rear] = element;
+    printf("%d enqueued to the queue\n", element);
+}
+
+int dequeue() {
+    int element;
+    if (isEmpty()) {
+        printf("Queue is empty. Cannot dequeue\n");
+        return -1;
+    }
+
+    element = queue[front];
+
+    if (front == rear) {
+        front = rear = -1;
+    } else {
+        front = (front + 1) % size;
+    }
+
+    printf("%d Dequeued \n", element);
+    return element;
+}
+int searchElement(int element) {
+    if (isEmpty()) {
+        printf("Queue is empty\n");
+        return -1;
+    }
+
+    int current = front;
+    int position = 1;
+
+    do {
+        if (queue[current] == element) {
+            return position;
+        }
+
+        current = (current + 1) % size;
+        position++;
+    } while (current != (rear + 1) % size);
+
+    return -1;
+}
+void displayQueue() {
+ if (isEmpty()) {
+   printf("Queue is empty\n");
+   return;
+  }
+
+ printf(" The Circular Queue elements: ");
+ int current = front;
+ do {
+    printf("%d ", queue[current]);
+     current = (current + 1) % size;
+    } while (current != (rear + 1) % size);
+
+    printf("\n");
 }
